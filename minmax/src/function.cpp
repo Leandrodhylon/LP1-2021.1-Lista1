009e48b7fdd11af99855a456458e1007f2837a66
Leandrodhylon <leandromagno14@gmail.com>
2021-11-07 08:04:57 -0300

    to com sono

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -1,5 +1,5 @@
 #include "function.h"
-
+using namespace std;
 /*! 
  * Finds and returns a pair with the first instance of the smallest element
  * and the last instance of the largest element in an array.
@@ -12,8 +12,20 @@
 
 std::pair<int,int> min_max( int V[], size_t n )
 {
-    // TODO: Adicione aqui sua solução.
-
-    // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+    int i=0,max=V[0],min=V[0], indmax=0, indmin=0;
+    if(n==0){
+        return {-1, -1};
+    }
+    while(i<n){
+        if(V[i]>=max){
+            indmax=i;
+            max=V[i];
+        }
+        if(V[i]<min){
+            indmin=i;
+            min=V[i];
+        }
+        i++;
+    }
+    return { indmin , indmax};
 }
