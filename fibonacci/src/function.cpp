44875c3cb49d363d2c9bfb3d2f728b4bfc24d8c7
Leandrodhylon <leandromagno14@gmail.com>
2021-11-07 06:15:04 -0300

    Quase difícil

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,20 @@
 #include "function.h"
+#include <vector>
+#include <iostream>
+using namespace std;
 
-std::vector<unsigned int> fib_below_n( unsigned int n )
-{
-    // TODO: adicione o seu código aqui.
-
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+std::vector<unsigned int> fib_below_n( unsigned int n){
+    vector<unsigned int> x;
+    int z=0, y=1, R=1;
+    if(n<2){
+    return x;
+    }
+    x.push_back(R); 
+    while(R<n){
+        x.push_back(R); 
+        z=y;
+        y=R;
+        R=z+y;         
+    } 
+    return x;
 }
