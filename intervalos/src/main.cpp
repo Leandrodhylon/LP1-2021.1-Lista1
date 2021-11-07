e57508a4691a21ce16629f8067053b5a7bb8eb84
Leandrodhylon <leandromagno14@gmail.com>
2021-11-07 03:44:34 -0300

    Ja tá ficando fácil

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -15,7 +15,31 @@ using std::setprecision;
 
 int main(void)
 {
-    // TODO: Adicione aqui a sua solução.
-
+    int i=0;
+    int x=0, valor;
+    double pri=0, seg=0, ter=0, qua=0, qui=0;
+    while( cin >> valor) {
+        if(valor>=0&&valor<25){
+            pri++;
+        }
+        if(valor>=25&&valor<50){
+            seg++;
+        }
+        if(valor>=50&&valor<75){
+            ter++;
+        }
+        if(valor>=75&&valor<100){
+            qua++;
+        }
+        if(valor<0||valor>=100){
+            qui++;
+        }
+        x++;
+    }
+    cout<<std::setprecision(4)<<100*pri/x<<endl;
+    cout<<std::setprecision(4)<<100*seg/x<<endl;
+    cout<<std::setprecision(4)<<100*ter/x<<endl;
+    cout<<std::setprecision(4)<<100*qua/x<<endl;
+    cout<<std::setprecision(4)<<100*qui/x<<endl;
     return 0;
 }
